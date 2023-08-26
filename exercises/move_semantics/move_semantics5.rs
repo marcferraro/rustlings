8a5e98944ef401f50da858c442642ce8082b8602
Marc Ferraro <marc.ferraro1@gmail.com>
2023-08-26 15:10:12 -0500

    32/96. vecs and move semantics

--- exercises/move_semantics/move_semantics5.rs
+++ exercises/move_semantics/move_semantics5.rs
@@ -1,3 +1,4 @@
+
 // move_semantics5.rs
 //
 // Make me compile only by reordering the lines in `main()`, but without adding,
@@ -6,13 +7,13 @@
 // Execute `rustlings hint move_semantics5` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
+
 
 fn main() {
     let mut x = 100;
     let y = &mut x;
-    let z = &mut x;
     *y += 100;
+    let z = &mut x;
     *z += 1000;
     assert_eq!(x, 1200);
 }
