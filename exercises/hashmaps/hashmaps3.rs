e0fe11de072912ad79046578dd50a186023c6a0b
Marc Ferraro <marc.ferraro1@gmail.com>
2023-08-30 14:15:38 -0500

    47/96 hashmaps

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,46 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+
+        // could clone the team name, but I don't see the need since we aren't
+        // using it later on
+        scores
+            .entry(team_1_name).and_modify(|team| {
+                team.goals_scored += team_1_score;
+                team.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score
+            });
+        scores
+            .entry(team_2_name).and_modify(|team| {
+                team.goals_scored += team_2_score;
+                team.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score
+            });
+        
+
+    //     let team_1 = scores.entry( ).or_insert(Team {
+    //         goals_scored: 0,
+    //         goals_conceded: 0,
+    //     });
+
+    //     team_1.goals_scored += team_1_score;
+    //     team_1.goals_conceded += team_2_score;
+
+    //     let team_2 = scores.entry(team_2_name).or_insert(Team {
+    //         goals_scored: 0,
+    //         goals_conceded: 0,
+    //     });
+
+    //     team_2.goals_scored += team_2_score;
+    //     team_2.goals_conceded += team_1_score;
+
+
     }
     scores
 }
